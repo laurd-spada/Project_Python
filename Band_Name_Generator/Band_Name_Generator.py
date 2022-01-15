0caa339adc245fb8763a2503bbb724e5e4aee4a2
laurd-spada <82173669+laurd-spada@users.noreply.github.com>
2022-01-15 16:43:16 +0100

    Band_name_generator

--- Band_Name_Generator/Band_Name_Generator.py
+++ Band_Name_Generator/Band_Name_Generator.py
@@ -1,1 +1,30 @@
-name = input("What is your name")
+from replit import clear
+# you may need to install the replit package to be able to use the clear() function
+from art import logo
+
+# the logo is imported from an external python file named art
+
+
+# The code will keep running in a loop until the user gives an "n" command
+Take_another_turn = True
+while Take_another_turn:
+    print(logo)
+    print("Welcome To Band Name Generator Program")
+    print("Generating names for bands")
+
+    User_City = input("What city did you grow up in? \n").lower()
+    User_Pet = input("What is the name of your favourite pet? \n").lower()
+    
+    Combination = User_City + User_Pet
+
+    print(f"The name of your band is called the {Combination}.")
+
+    Re_do = input("Do you wand to generate another name, 'y' for yes or 'n' for no ").lower()
+    # if the response selected is either "y" or "n", the if statement would run
+    if Re_do == "y":
+        clear()
+        Take_another_turn = True
+
+    else:
+        clear()
+        Take_another_turn = False
---
+++ Band_Name_Generator/art.py
@@ -0,0 +1,12 @@
+logo = '''
+
+88888888ba                                  88 888b      88                                            ,ad8888ba,                                                                                         
+88      "8b                                 88 8888b     88                                           d8"'    `"8b                                                           ,d                           
+88      ,8P                                 88 88 `8b    88                                          d8'                                                                     88                           
+88aaaaaa8P' ,adPPYYba, 8b,dPPYba,   ,adPPYb,88 88  `8b   88 ,adPPYYba, 88,dPYba,,adPYba,   ,adPPYba, 88             ,adPPYba, 8b,dPPYba,   ,adPPYba, 8b,dPPYba, ,adPPYYba, MM88MMM ,adPPYba,  8b,dPPYba,  
+88""""""8b, ""     `Y8 88P'   `"8a a8"    `Y88 88   `8b  88 ""     `Y8 88P'   "88"    "8a a8P_____88 88      88888 a8P_____88 88P'   `"8a a8P_____88 88P'   "Y8 ""     `Y8   88   a8"     "8a 88P'   "Y8  
+88      `8b ,adPPPPP88 88       88 8b       88 88    `8b 88 ,adPPPPP88 88      88      88 8PP""""""" Y8,        88 8PP""""""" 88       88 8PP""""""" 88         ,adPPPPP88   88   8b       d8 88          
+88      a8P 88,    ,88 88       88 "8a,   ,d88 88     `8888 88,    ,88 88      88      88 "8b,   ,aa  Y8a.    .a88 "8b,   ,aa 88       88 "8b,   ,aa 88         88,    ,88   88,  "8a,   ,a8" 88          
+88888888P"  `"8bbdP"Y8 88       88  `"8bbdP"Y8 88      `888 `"8bbdP"Y8 88      88      88  `"Ybbd8"'   `"Y88888P"   `"Ybbd8"' 88       88  `"Ybbd8"' 88         `"8bbdP"Y8   "Y888 `"YbbdP"'  88          
+
+'''
